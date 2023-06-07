2b2fa0cdc32f0f434010470da81b9fc3424aba5c
Tuan Vo <tuanvo2908t@gmail.com>
2023-06-07 13:22:31 +0700

    add env

---
+++ lib/.env.dart
@@ -0,0 +1,6 @@
+
+const publishable_key =
+    "pk_test_51N6pjfJjOgMxOgVuOUaXzf9VAReJKHOLgDgQr2kyxM1I055VmjZCJ2WMDA4EB2A0pjjGYr0atHbWU3Cw2fzCFS0y00QcaPZU1G";
+const secret_key =
+    "sk_test_51N6pjfJjOgMxOgVuIckY4TcIzo2KHIsT540kwHIjmemQ5ISKcBRDOnMvLv4aZA5d1Cxmj2FEVe3FgdDdKZ1FVakv00qT69qCPx";
+const customerId = 'cus_O1WLT448ynbR9N';
